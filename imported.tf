e9dab6e6497de584c1279f825a2229b6ea93a505
Fabian Borges <fabian@valuetainment.com>
2025-01-07 13:20:52 -0500

    Initial commit

---
+++ imported.tf
@@ -0,0 +1,48 @@
+##################################################################################
+# IMPORTS
+##################################################################################
+
+# import {
+#   to = module.main.aws_vpc.this[0]
+#   id = "vpc-08edb34517bc3b06e" #VPC
+# }
+
+# import {
+#   to = module.main.aws_subnet.public[0]
+#   id = "subnet-09b815e03676dba59" #PublicSubnet1
+# }
+
+# import {
+#   to = module.main.aws_subnet.public[1]
+#   id = "subnet-0226f88a69ce3737b" #PublicSubnet2
+# }
+
+# import {
+#   to = module.main.aws_internet_gateway.this[0]
+#   id = "igw-0eda0ffcca0282be2" #InternetGateway
+# }
+
+# import {
+#   to = module.main.aws_route.public_internet_gateway[0]
+#   id = "rtb-03e2a2e073dc679fe_0.0.0.0/0" #DefaultPublicRoute
+# }
+
+# import {
+#   to = module.main.aws_route_table.public[0]
+#   id = "rtb-03e2a2e073dc679fe" #PublicRouteTable
+# }
+
+# import {
+#   to = module.main.aws_route_table_association.public[0]
+#   id = "subnet-09b815e03676dba59/rtb-03e2a2e073dc679fe" #PublicSubnet1/PublicRouteTable
+# }
+
+# import {
+#   to = module.main.aws_route_table_association.public[1]
+#   id = "subnet-0226f88a69ce3737b/rtb-03e2a2e073dc679fe" #PublicSubnet2/PublicRouteTable
+# }
+
+# import {
+#   to = aws_security_group.ingress
+#   id = "sg-04bc5782289dffed4" #NoIngressSecurityGroup
+# }
